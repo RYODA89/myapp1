8e1b2a662a969507c1a32e7eba875306a58794ff
RYODA <yodarazak@gmail.com>
2024-11-04 12:07:30 +0000

    first commit workflow2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test https://ryoda.demo-server-spider.com/myapp1
         </a>
       </header>
     </div>
